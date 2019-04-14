2d1db37c2feb004a1ab5ad011253819da96d5b96
NouranAmr <nouranamr206@gmail.com>
2019-04-14 05:26:08 -0700

    HelloDemo commit

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Esraa Hassan on 4/9/19.
+//  Copyright © 2019 Esraa And Passant. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    printf("%s \n","HelloObjectiveC");
+    NSLog(@"\n HelloWorld");
+    return 0;
+}
