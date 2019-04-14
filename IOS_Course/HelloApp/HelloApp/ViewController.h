93ec20d41fd245ba7821ed95dc117ea27617408b
NouranAmr <nouranamr206@gmail.com>
2019-04-14 05:24:53 -0700

    seventh commit

---
+++ IOS_Course/HelloApp/HelloApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  HelloApp
+//
+//  Created by Esraa Hassan on 4/11/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)updateBtn:(id)sender;
+
+
+@end
+
---
+++ IOS_Course/HelloApp/HelloApp/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  HelloApp
+//
+//  Created by Esraa Hassan on 4/11/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)updateBtn:(id)sender {
+}
+@end
